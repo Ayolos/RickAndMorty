fe21325f1f4b4e4690480411e338a82ac9c8b793
Antoine <49106208+Ayolos@users.noreply.github.com>
2022-03-28 19:14:41 +0200

    fix bug

--- src/components/Rick/Accueil.vue
+++ src/components/Rick/Accueil.vue
@@ -1,21 +1,28 @@
 <template>
-    <h3>PAGE ACCUEIL</h3>
+<div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8 align-item-center">
+      <div class="card-body">
+        <button type="button" class="btn-close" aria-label="Close"></button>
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
-import axios from "axios";
 
 export default defineComponent({
   data: () => {
     return {
-      pokemon: [],
     };
-  },
-  mounted() {
-    axios.get("https://pokeapi.co/api/v2/pokemon", {}).then((reponse) => {
-      this.pokemon = reponse.data;
-    });
   },
 });
 </script>
